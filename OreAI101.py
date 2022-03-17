825554c1e6669b298f3b09ef086f4f623eea1b46
ore2000 <59839438+ore2000@users.noreply.github.com>
2022-03-17 09:53:27 -0600

    Update OreAI101.py

--- OreAI101.py
+++ OreAI101.py
@@ -2,6 +2,12 @@
 """
 Created on Thu Mar 17 09:45:53 2022
 
-@author: tjuli
-"""
+@author: Oreoluwa Adeola Babatunde
 
+This code is to help easily get into machine learning programming without having to use things like tensorflow with huge dependencies.
+
+"""
+import numpy
+import math
+
+print(numpy.__version__)
